cd37f1dc6fb71a75ec7dc38b4f64a73e9f01b72a
BohdanDem <bogdandemchuk.1@gmail.com>
2023-08-31 20:01:31 +0300

    lesson_1_intro

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+import {IUser} from "../../interfaces";
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  users: IUser[]
+
+  ngOnInit() {
+  }
+
+}
